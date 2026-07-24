89da31d5145e80be14f817136d4f00e063d82e9f
edm28contact-wq <edm28.contact@gmail.com>
2026-07-24 20:44:17 +0200

    feat: load client messaging loop

--- client-simple-flow.js
+++ client-simple-flow.js
@@ -10,7 +10,7 @@
     { src: '/client-operations.js?v=1', attr: 'data-edm-operations' },
     { src: '/client-invoices.js?v=1', attr: 'data-edm-invoices' },
     { src: '/client-document-download.js?v=1', attr: 'data-edm-document-downloads' },
-    { src: '/client-notifications.js?v=1', attr: 'data-edm-notifications' },
+    { src: '/client-messages.js?v=1', attr: 'data-edm-messages' },
     { src: '/combo-suspended.js?v=1', attr: 'data-edm-combo-policy' }
   ];
 
