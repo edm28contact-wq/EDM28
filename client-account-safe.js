52e1475d8cf88476052d1424d1cba674881226b7
edm28contact-wq <edm28.contact@gmail.com>
2026-07-24 20:45:18 +0200

    feat: guard client messaging route

--- client-account-safe.js
+++ client-account-safe.js
@@ -33,7 +33,7 @@
     if (typeof saveState === 'function') saveState();
   };
 
-  const protectedPages = new Set(['account', 'garage', 'history']);
+  const protectedPages = new Set(['account', 'garage', 'history', 'messages']);
   const baseShowPage = window.showPage;
   if (typeof baseShowPage === 'function') {
     window.showPage = function (pageId) {
