2cf9e0d07f2ffb6ab7c599ca8b423e2cf140065a
edm28contact-wq <edm28.contact@gmail.com>
2026-07-27 22:17:01 +0200

    Charger l’aide sur les notes client

--- client-simple-flow.js
+++ client-simple-flow.js
@@ -13,7 +13,8 @@
     { src: '/client-document-download.js?v=1', attr: 'data-edm-document-downloads' },
     { src: '/client-messages.js?v=1', attr: 'data-edm-messages' },
     { src: '/combo-suspended.js?v=1', attr: 'data-edm-combo-policy' },
-    { src: '/client-basket-pricing.js?v=1', attr: 'data-edm-basket-pricing' }
+    { src: '/client-basket-pricing.js?v=1', attr: 'data-edm-basket-pricing' },
+    { src: '/client-note-guidance.js?v=1', attr: 'data-edm-note-guidance' }
   ];
 
   const loadScript = ({ src, attr }) => new Promise((resolve, reject) => {
